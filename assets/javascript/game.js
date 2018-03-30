f56b4b96323b6b9c66bff2e9da27709bb30a5ae9
larkenem <larken.mikeal@gmail.com>
2018-03-29 20:59:17 -0600

    beginning stages

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+//letters the computer chooses
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//variables for user guess, wins, losses and letters already guessed
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+
+//place to store user and computer guess
+var userGuess = "";
+var computerGuess = "";
+
+
+
+
